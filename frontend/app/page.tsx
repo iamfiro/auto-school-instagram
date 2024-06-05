52c66c63ad5c1bc74f79ddb50cb8fed449a2d20a
FIRO <appturbo102@gmail.com>
2024-06-05 21:12:40 +0900

    feat: add Board component to Home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import style from "@/styles/page.module.scss";
 import { Header } from "@/components";
+import Board from "@/components/Board";
 
 export default function Home() {
     return (
@@ -13,7 +13,28 @@ export default function Home() {
                     <Header.CreateMeal />
                 </Header.Box>
             </Header>
-            asd
+            <section className={style.boardContainer}>
+                <Board date={new Date()}>
+                    <Board.Meal name="백미밥" code={[]} />
+                    <Board.Meal name="메밀소바" code={[3,5,6,7,13]} />
+                    <Board.Meal name="등심돈까스" code={[1,2,5,6,10,12,16]} />
+                    <Board.Meal name="샐러드" code={[13]} />
+                    <Board.Meal name="배추김치" code={[9]} />
+                    <Board.Meal name="메밀소바" code={[3,5,6,7,13]} />
+                    <Board.Meal name="초코프리첼" code={[2,5,6]} />
+                    <Board.Meal name="메밀소바" code={[13]} />
+                </Board>
+                <Board date={new Date()}>
+                    <Board.Meal name="백미밥" code={[]} />
+                    <Board.Meal name="메밀소바" code={[3,5,6,7,13]} />
+                    <Board.Meal name="등심돈까스" code={[1,2,5,6,10,12,16]} />
+                    <Board.Meal name="샐러드" code={[13]} />
+                    <Board.Meal name="배추김치" code={[9]} />
+                    <Board.Meal name="메밀소바" code={[3,5,6,7,13]} />
+                    <Board.Meal name="초코프리첼" code={[2,5,6]} />
+                    <Board.Meal name="메밀소바" code={[13]} />
+                </Board>
+            </section>
         </main>
     );
 }
