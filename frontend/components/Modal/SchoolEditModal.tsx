9014ffe9b42691afc81ac693ab0fc026e802c17f
FIRO <appturbo102@gmail.com>
2024-06-06 23:10:59 +0900

    refactor: Update school selection form in ModalSchoolSetting

--- frontend/components/Modal/SchoolEditModal.tsx
+++ frontend/components/Modal/SchoolEditModal.tsx
@@ -2,6 +2,8 @@ import Modal from "./common";
 import { TbSchool } from "react-icons/tb";
 import style from "./modal.module.scss";
 import { useState } from "react";
+import Select from "react-select";
+import { koreaEducationOffice } from "@/constant/koreaEducationOffice";
 
 function ModalSchoolSetting() {
     const [disabled, setDisabled] = useState(true);
@@ -9,19 +11,18 @@ function ModalSchoolSetting() {
         <Modal.Backdrop>
             <Modal>
                 <Modal.Top icon={<TbSchool />} color="#2fcc88">학교 설정</Modal.Top>
-                <div className={style.selectedSchool}>
-                    <h2>설정된 학교</h2>
-                    <div>
-                        <TbSchool size={18} />
-                        <span>선린인터넷고등학교</span>
-                    </div>
+                <div className={style.form} id="">
+                    <label htmlFor="educationOffice" className={style.label}>교육청</label>
+                    <select name="educationOffice" id="educationOffice" className={style.option}>
+                        {
+                            koreaEducationOffice.map((office, index) => {
+                                return <option key={index} value={office.value}>{office.label}</option>
+                            })
+                        }
+                    </select>
+                    <label htmlFor="schoolName" className={style.label}>학교 교육청</label>
+                    <input id="schoolName" placeholder="학교 이름을 입력해주세요" className={style.input}></input>
                 </div>
-                <select name="code" id="code">
-                    <option value="1">서울특별시교육청</option>
-                    <option value="2">부산광역시교육청</option>
-                    <option value="3">선린인터넷고등학교</option>
-                    <option value="4">선린인터넷고등학교</option>
-                </select>
                 <button className={style.submit} disabled={disabled}>눌러서 학교 수정하기</button>
             </Modal>
         </Modal.Backdrop>
