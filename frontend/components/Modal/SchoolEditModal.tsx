18536b70e7931f0cea651bf97507cdd6cd57a5a2
FIRO <appturbo102@gmail.com>
2024-06-06 22:45:57 +0900

    refactor: Rename ModalSchoolEdit to ModalSchoolSetting and add school selection dropdown

--- frontend/components/Modal/SchoolEditModal.tsx
+++ frontend/components/Modal/SchoolEditModal.tsx
@@ -1,12 +1,14 @@
 import Modal from "./common";
 import { TbSchool } from "react-icons/tb";
 import style from "./modal.module.scss";
+import { useState } from "react";
 
-function ModalSchoolEdit() {
+function ModalSchoolSetting() {
+    const [disabled, setDisabled] = useState(true);
     return (
         <Modal.Backdrop>
             <Modal>
-                <Modal.Top icon={<TbSchool />} color="#2fcc88">학교 수정하기</Modal.Top>
+                <Modal.Top icon={<TbSchool />} color="#2fcc88">학교 설정</Modal.Top>
                 <div className={style.selectedSchool}>
                     <h2>설정된 학교</h2>
                     <div>
@@ -14,10 +16,16 @@ function ModalSchoolEdit() {
                         <span>선린인터넷고등학교</span>
                     </div>
                 </div>
-                <button className={style.submit}>눌러서 학교 수정하기</button>
+                <select name="code" id="code">
+                    <option value="1">서울특별시교육청</option>
+                    <option value="2">부산광역시교육청</option>
+                    <option value="3">선린인터넷고등학교</option>
+                    <option value="4">선린인터넷고등학교</option>
+                </select>
+                <button className={style.submit} disabled={disabled}>눌러서 학교 수정하기</button>
             </Modal>
         </Modal.Backdrop>
     );
 }
 
-export default ModalSchoolEdit;
+export default ModalSchoolSetting;
