50a284c132aa4e52669f285d67c5879d88e1cc98
FIRO <appturbo102@gmail.com>
2024-06-06 22:49:54 +0900

    feat: Add Korea Education Office constant array

---
+++ frontend/constant/koreaEducationOffice.ts
@@ -0,0 +1,19 @@
+const koreaEducationOffice: Array<{ name: string, value: string }> = [
+    { name: '서울특별시교육청', value: '1' },
+    { name: '부산광역시교육청', value: '2' },
+    { name: '대구광역시교육청', value: '3' },
+    { name: '인천광역시교육청', value: '4' },
+    { name: '광주광역시교육청', value: '5' },
+    { name: '대전광역시교육청', value: '6' },
+    { name: '울산광역시교육청', value: '7' },
+    { name: '세종특별자치시교육청', value: '8' },
+    { name: '경기도교육청', value: '9' },
+    { name: '강원도교육청', value: '10' },
+    { name: '충청북도교육청', value: '11' },
+    { name: '충청남도교육청', value: '12' },
+    { name: '전라북도교육청', value: '13' },
+    { name: '전라남도교육청', value: '14' },
+    { name: '경상북도교육청', value: '15' },
+    { name: '경상남도교육청', value: '16' },
+    { name: '제주특별자치도교육청', value: '17' }
+]
